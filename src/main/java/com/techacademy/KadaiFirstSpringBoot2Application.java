2ae7b579559dfa1b6025ac9ce1b54b1bb09c26dd
sayando <andousayuka@gmail.com>
2023-04-26 19:44:56 +0900

    add

---
+++ src/main/java/com/techacademy/KadaiFirstSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadaiFirstSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadaiFirstSpringBoot2Application.class, args);
+	}
+
+}
